e18972b10c661df6b26f5b02d79143ac5e2152eb
SEMA <janchevsem@mail.ru>
2021-10-13 15:30:48 +0500

    admin-initial

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::group(['prefix' => 'admin', 'namespace' => 'Admin'], function () {
+    Route::get('/', 'MainController@index')->name('admin.index');
+});
