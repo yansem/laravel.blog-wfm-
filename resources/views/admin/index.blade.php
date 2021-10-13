e0ca4300a4cb3d833c830bf00b6600edd1f2f3e8
SEMA <janchevsem@mail.ru>
2021-10-13 19:47:59 +0500

    admin-npm install

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -7,13 +7,7 @@
     <!-- Tell the browser to be responsive to screen width -->
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <!-- Font Awesome -->
-    <link rel="stylesheet" href="../../plugins/fontawesome-free/css/all.min.css">
-    <!-- Ionicons -->
     <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
-    <!-- overlayScrollbars -->
-    <link rel="stylesheet" href="../../dist/css/adminlte.min.css">
-    <!-- Google Font: Source Sans Pro -->
     <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
 </head>
 <body class="hold-transition sidebar-mini">
@@ -767,13 +761,5 @@
 </div>
 <!-- ./wrapper -->
 
-<!-- jQuery -->
-<script src="../../plugins/jquery/jquery.min.js"></script>
-<!-- Bootstrap 4 -->
-<script src="../../plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
-<!-- AdminLTE App -->
-<script src="../../dist/js/adminlte.min.js"></script>
-<!-- AdminLTE for demo purposes -->
-<script src="../../dist/js/demo.js"></script>
 </body>
 </html>
--- webpack.mix.js
+++ webpack.mix.js
@@ -15,3 +15,18 @@ mix.js('resources/js/app.js', 'public/js')
     .postCss('resources/css/app.css', 'public/css', [
         //
     ]);
+
+mix.styles([
+    'resources/assets/admin/plugins/fontawesome-free/css/all.min.css',
+    'resources/assets/admin/css/adminlte.min.css'
+], 'public/assets/admin/css/admin.css');
+
+mix.scripts([
+    'resources/assets/admin/plugins/jquery/jquery.min.js',
+    'resources/assets/admin/plugins/bootstrap/js/bootstrap.bundle.min.js',
+    'resources/assets/admin/js/adminlte.min.js',
+    'resources/assets/admin/js/demo.js'
+], 'public/assets/admin/js/admin.js');
+
+mix.copy('resources/assets/admin/img', 'public/assets/admin/img');
+mix.copy('resources/assets/admin/plugins/fontawesome-free/webfonts', 'public/assets/admin/css/webfonts');
