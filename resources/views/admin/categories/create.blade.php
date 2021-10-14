77ebd6b416fc40fba95f3bdb47f415ac2db55cc2
SEMA <janchevsem@mail.ru>
2021-10-14 19:59:19 +0500

    admin-create form

---
+++ resources/views/admin/categories/create.blade.php
@@ -0,0 +1,43 @@
+@extends('admin.layouts.layout')
+
+@section('content')
+        <!-- Content Header (Page header) -->
+        <section class="content-header">
+            <div class="container-fluid">
+                <div class="row mb-2">
+                    <div class="col-sm-6">
+                        <h1>Категории</h1>
+                    </div>
+                    <div class="col-sm-6">
+                        <ol class="breadcrumb float-sm-right">
+                            <li class="breadcrumb-item"><a href="#">Home</a></li>
+                            <li class="breadcrumb-item active">Blank Page</li>
+                        </ol>
+                    </div>
+                </div>
+            </div><!-- /.container-fluid -->
+        </section>
+
+        <!-- Main content -->
+        <form role="form" method="post" action="{{ route('categories.store') }}">
+            @csrf
+            <div class="card-body">
+                <div class="form-group">
+                    <label for="title">Title</label>
+                    <input type="text" class="form-control @error('title') is-invalid @enderror" name="title"
+                           id="title"
+                           placeholder="Enter title">
+                    @error('title')
+                    <div class="invalid-feedback">{{ $message }}</div>
+                    @enderror
+                </div>
+            </div>
+            <!-- /.card-body -->
+
+            <div class="card-footer">
+                <button type="submit" class="btn btn-primary">Submit</button>
+            </div>
+        </form>
+        <!-- /.content -->
+@endsection
+
