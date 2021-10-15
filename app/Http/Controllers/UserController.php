077a7080f8de6fa216f03fe1fce7f4391a40ecc8
SEMA <janchevsem@mail.ru>
2021-10-15 16:50:15 +0500

    admin-register controller

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function create()
+    {
+        return view('user.create');
+    }
+
+    public function store(Request $request)
+    {
+        return $request->all();
+    }
+}
